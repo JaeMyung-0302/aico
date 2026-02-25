cf9c7e8d7bf34d2194ccaffbd188faf5fe0ebcd8
JaeMyung-Lattice <jaemyung@lattice.im>
2026-02-25 09:37:06 +0900

    modify error

--- apps/soulblade/server/src/main.ts
+++ apps/soulblade/server/src/main.ts
@@ -32,4 +32,7 @@ const bootstrap = async () => {
   Logger.log(`SoulBlade API running on http://localhost:${port}`, 'Bootstrap');
 };
 
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Fatal bootstrap error:', error);
+  process.exit(1);
+});
